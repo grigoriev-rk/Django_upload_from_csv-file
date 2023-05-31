38aa0dd699e02a4182fc5be6654744a2297c7fe5
Roman Grigoriev <108183544+grigoriev-rk@users.noreply.github.com>
2023-05-31 21:00:58 +0300

    Update models.py

--- phones/models.py
+++ phones/models.py
@@ -3,10 +3,11 @@ from datetime import datetime
 
 
 class Phone(models.Model):
-    id = models.AutoField(primary_key=True)
+
+    id = models.IntegerField(primary_key=True, default=None)
     name = models.CharField(max_length=50, default=False)
     price = models.IntegerField(default=0)
-    image = models.CharField()
+    image = models.URLField(default=None)
     release = models.DateField(default=datetime.now().strftime("%Y-%m-%d"))
     lte_exists = models.BooleanField(default=0)
     slug = models.SlugField(max_length=100, unique=True)
