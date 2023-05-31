75f7d3211c0d847826c7dcbdeccccb4836e11ab1
Roman Grigoriev <108183544+grigoriev-rk@users.noreply.github.com>
2023-05-31 21:01:29 +0300

    Update import_phones.py

--- phones/management/commands/import_phones.py
+++ phones/management/commands/import_phones.py
@@ -13,15 +13,13 @@ class Command(BaseCommand):
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-            phone_list = Phone.objects.filter(id=phone['id'])
-            if not phone_list:
-                Phone.objects.create(
-                    id=phone['id'],
-                    name=phone['name'],
-                    price=phone['price'],
-                    image=phone['image'],
-                    release=phone['release_date'],
-                    lte_exists=phone['lte_exists'],
-                    slug=(phone['name'])
+            Phone.objects.create(
+                id=phone['id'],
+                name=phone['name'],
+                price=phone['price'],
+                image=phone['image'],
+                release=phone['release_date'],
+                lte_exists=phone['lte_exists'],
+                slug=(phone['name'])
                 )
-                phone_list.save()
+                #phone_list.save()
